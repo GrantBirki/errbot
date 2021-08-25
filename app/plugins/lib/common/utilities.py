b6eb7babe90402f449fdcf31a6db5981d988ce10
Grant Birkinbine <grantbirki@github.com>
2021-08-24 20:10:37 -0700

    sha256 util

--- app/plugins/lib/common/utilities.py
+++ app/plugins/lib/common/utilities.py
@@ -1,3 +1,4 @@
+import hashlib
 from datetime import datetime
 
 class Util:
@@ -5,4 +6,13 @@ class Util:
     A collection of common utilities used throughout the repo
     """
     def iso_timestamp(self):
+        """
+        Return a ISO formatted timestamp of the current time
+        """
         return datetime.now().replace(microsecond=0).isoformat()
+
+    def sha256_hash(self, data):
+        """
+        Hash a string and return a SHA256 hash
+        """
+        return hashlib.sha256(data.encode('utf-8')).hexdigest()
