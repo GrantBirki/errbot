dc093998b56e6467ab541a63aed49a2cad6370c5
Grant Birkinbine <grantbirki@github.com>
2021-08-25 20:05:12 -0700

    manual add to league watcher with dynamo

--- app/plugins/league/league.py
+++ app/plugins/league/league.py
@@ -102,7 +102,8 @@ class League(BotPlugin):
 
     @arg_botcmd('--summoner_name', dest='summoner_name', type=str, admin_only=True)
     @arg_botcmd('--discord_handle', dest='discord_handle', type=str, admin_only=True)
-    def add_to_league_watcher(self, msg, summoner_name=None, discord_handle=None):
+    @arg_botcmd('--guild', dest='guild', type=int, admin_only=True)
+    def add_to_league_watcher(self, msg, summoner_name=None, discord_handle=None, guild=None):
         """
         Adds a summoner to the league watcher - Admin command
         Run from the channel you wish to use the league watcher in
@@ -110,24 +111,26 @@ class League(BotPlugin):
         Usage: .add to league watcher <summoner_name> <discord_handle>
         Example: .add to league watcher --summoner_name birki --discord_handle birki#0001
         """
-        guild_id, guild_msg = discord.guild_id(msg)
+        guild_id = int(guild)
 
-        if not guild_id:
-            return guild_msg
+        get_result = dynamo.get(LeagueTable, guild_id, discord_handle)
+        if get_result:
+            return f"ℹ️ {discord.mention_user(msg)} already has an entry in the league watcher!"
+        elif get_result is False:
+            return f"❌ Failed to check the league watcher for {discord.mention_user(msg)}"
 
-        result = cosmos.create_items(
-            id = discord_handle,
-            data = {
-                'discord_handle': discord_handle,
-                'summoner_name': summoner_name,
-                'discord_server_id': guild_id
-            }
+        write_result = dynamo.write(
+            LeagueTable(
+                discord_server_id=guild_id,
+                discord_handle=discord_handle,
+                summoner_name=summoner_name
+            )
         )
 
-        if result:
-            return f"✅ Added `{discord_handle}` to the league watcher! | Summoner Name: `{summoner_name}`"
+        if write_result:
+            return f"✅ Added {discord.mention_user(msg)} to the league watcher!"
         else:
-            return f"ℹ️ `{discord_handle}` already has an entry in the league watcher!"
+            return f"❌ Faile to add {discord.mention_user(msg)} to the league watcher!"
 
     @botcmd
     def remove_me_from_league_watcher(self, msg, args):
