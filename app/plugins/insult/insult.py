0186685ff2d903f2f23089149721214802d90b1b
Grant Birkinbine <grantbirki@github.com>
2021-08-26 23:38:07 -0700

    insult a user

--- app/plugins/insult/insult.py
+++ app/plugins/insult/insult.py
@@ -8,4 +8,15 @@ class Insult(BotPlugin):
         """
         Get insulted to reset your confidence
         """
+        return self.insult()
+    
+    @botcmd
+    def insult(self, msg, args):
+        """
+        Insult a specifc user
+        Example: .insult @user
+        """
+        return f"{args} {self.get_insult()}"
+
+    def get_insult(self):
         return requests.get('https://evilinsult.com/generate_insult.php?lang=en&type=json').json()['insult']
