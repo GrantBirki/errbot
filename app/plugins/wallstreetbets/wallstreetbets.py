05280e59485b446507e9f5af9300bbb3e0b3ead0
Grant Birkinbine <grantbirki@github.com>
2021-08-26 23:58:41 -0700

    wallstreetbets plugin

---
+++ app/plugins/wallstreetbets/wallstreetbets.py
@@ -0,0 +1,24 @@
+from errbot import BotPlugin, botcmd
+import requests
+
+class Wallstreetbets(BotPlugin):
+    """Wallstreetbets plugin for Errbot"""
+
+    @botcmd
+    def wallstreetbets(self, msg, args):
+        """
+        Get the top stonks from Wallstreetbets and their sentiment
+        """
+
+        stonks = self.get_wallstreebets()[:5]
+
+        message = []
+        for stonk in stonks:
+            message.append(
+                f"Ticker: `${stonk['ticker'].ljust(4)}` | Sentiment: `{stonk['sentiment']}` | Comments: `{stonk['no_of_comments']}`"
+            )
+
+        return '\n'.join(message)
+
+    def get_wallstreebets(self):
+        return requests.get('https://dashboard.nbshare.io/api/v1/apps/reddit').json()
