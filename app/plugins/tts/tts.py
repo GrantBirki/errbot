a198b9913f5daae08262f9ada0d938927a555c1d
Grant Birkinbine <grantbirki@github.com>
2021-10-26 23:36:08 -0700

    init working version of tts file

---
+++ app/plugins/tts/tts.py
@@ -0,0 +1,131 @@
+from errbot import BotPlugin, botcmd
+import asyncio
+import websockets
+import json
+import os
+
+DEFAULT_INTENT = 32460
+
+DISPATCH = 0
+HEARTBEAT = 1
+IDENTIFY = 2
+STATUS_UPDATE = 3
+VOICE_UPDATE = 4
+RESUME = 6
+RECONNECT = 7
+REQUEST_MEMBERS = 8
+INVALID_SESSION = 9
+HELLO = 10
+HEARTBEAT_ACK = 11
+
+VOICE_CHANNEL = 873463331917299726
+GUILD_ID = 873463331917299722
+
+TOKEN = os.environ['CHAT_SERVICE_TOKEN']
+
+class Tts(BotPlugin):
+    """Tts plugin for Errbot"""
+
+    @botcmd
+    def tts(self, msg, args):
+        """
+        The most basic example of a chatbot command/function
+        Tip: The name of the function above is literally how you invoke the chatop: .tts
+        Pro Tip: "_" in function names render as spaces so you can do 'def send_tts(...)' -> .send tts
+        """
+
+        Bot()
+
+        # Return a message / output below
+        return "test"
+
+class Bot:
+    def __init__(self):
+        self.interval = None
+        self.sequence = None
+        self.session_id = None
+
+        self.auth = {
+            "token": TOKEN,
+            "properties": {"$os": "linux", "$browser": "python", "$device": "errbot"},
+            "intents": DEFAULT_INTENT,
+            "presence": {
+                "game": {"name": "testing", "type": 0},
+                "status": "online",
+                "afk": False,
+            },
+        }
+
+        asyncio.run(self.main())
+
+    async def main(self):
+        async with websockets.connect(
+            "wss://gateway.discord.gg/?v=9&encoding=json"
+        ) as self.websocket:
+            await self.hello()
+            if self.interval is None:
+                print("Hello failed, exiting")
+                return
+            payload = {
+                "guild_id": GUILD_ID,
+                "channel_id": VOICE_CHANNEL,
+                "self_mute": False,
+                "self_deaf": False,
+            }
+            await asyncio.gather(
+                self.heartbeat(), self.receive(), self.send(4, payload)
+            )
+
+    async def receive(self):
+        print("Entering receive")
+        async for message in self.websocket:
+            print("<", message)
+            data = json.loads(message)
+            if data["op"] == DISPATCH:
+                self.sequence = int(data["s"])
+                event_type = data["t"]
+                if event_type == "READY":
+                    self.session_id = data["d"]["session_id"]
+                    print("Got session ID:", self.session_id)
+                elif event_type == "MESSAGE_CREATE":
+                    message = data["d"]["content"]
+                    if message.startswith("!edit"):
+                        print("Edit")
+                        parts = message.split()
+                        if len(parts) != 2:
+                            print("Command error")
+                        else:
+                            id = parts[1]
+                            print("id", id)
+
+    async def send(self, opcode, payload):
+        data = self.opcode(opcode, payload)
+        print(">", data)
+        await self.websocket.send(data)
+
+    async def heartbeat(self):
+        print("Entering heartbeat")
+        while self.interval is not None:
+            print("Sending a heartbeat")
+            await self.send(HEARTBEAT, self.sequence)
+            await asyncio.sleep(self.interval)
+
+    async def hello(self):
+        await self.send(IDENTIFY, self.auth)
+        print(f"hello > auth")
+
+        ret = await self.websocket.recv()
+        print(f"hello < {ret}")
+
+        data = json.loads(ret)
+        opcode = data["op"]
+        if opcode != 10:
+            print("Unexpected reply")
+            print(ret)
+            return
+        self.interval = (data["d"]["heartbeat_interval"] - 2000) / 1000
+        print("interval:", self.interval)
+
+    def opcode(self, opcode: int, payload) -> str:
+        data = {"op": opcode, "d": payload}
+        return json.dumps(data)
