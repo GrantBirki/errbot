f7d2e410c9e7518925124e512db5d0d1149cc04d
Grant Birkinbine <grant.birkinbine@gmail.com>
2021-11-04 14:14:57 -0700

    terraform sparkle changes

--- terraform/dynamoDB_tables.tf
+++ terraform/dynamoDB_tables.tf
@@ -106,3 +106,29 @@ module "dynamodb_table_tts" {
   }
 }
 
+# For the Sparkle commands
+module "dynamodb_table_sparkle" {
+  source  = "terraform-aws-modules/dynamodb-table/aws"
+  version = "1.1.0"
+
+  name                           = "sparkle"
+  hash_key                       = "discord_server_id"
+  range_key                      = "discord_handle"
+  billing_mode                   = "PAY_PER_REQUEST"
+  point_in_time_recovery_enabled = true
+
+  attributes = [
+    {
+      name = "discord_server_id"
+      type = "N"
+    },
+    {
+      name = "discord_handle"
+      type = "S"
+    },
+  ]
+
+  tags = {
+    managed_by = "terraform"
+  }
+}
