65dd8c974690d41321008f23bd96945834ff48b8
Grant Birkinbine <grantbirki@github.com>
2021-11-22 22:27:01 -0800

    add sentry capture method

--- app/plugins/lib/common/sentry.py
+++ app/plugins/lib/common/sentry.py
@@ -1,4 +1,4 @@
-from sentry_sdk import set_user
+from sentry_sdk import set_user, capture_exception
 from lib.chat.discord import Discord
 
 discord = Discord()
@@ -14,5 +14,15 @@ class Sentry:
         Set the Sentry context for a user
 
         Simply call Sentry().user(msg) to set the context for a user from any @botcmd function
+        :param msg: The message object from the @botcmd function
+        :return: None
         """
         set_user({"username": discord.handle(msg)})
+    
+    def capture(self, error):
+        """
+        Capture an exception and send it to Sentry
+        :param error: The exception to be captured
+        :return: None
+        """
+        capture_exception(error)
