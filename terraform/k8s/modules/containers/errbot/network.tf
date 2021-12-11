58480c3c1cec32e4ee3701f2e0a6509d5e19f436
Grant Birkinbine <grantbirki@github.com>
2021-12-10 23:22:28 -0800

    deny all traffic to errbot

---
+++ terraform/k8s/modules/containers/errbot/network.tf
@@ -0,0 +1,15 @@
+data "kubectl_path_documents" "errbot_network_policy_manifest" {
+  depends_on = [
+    data.kubectl_file_documents.errbot_namespace_manifest
+  ]
+
+  pattern = "modules/containers/errbot/network.yaml"
+}
+
+resource "kubectl_manifest" "errbot_network_policy" {
+  depends_on = [
+    data.kubectl_file_documents.errbot_namespace_manifest
+  ]
+  count     = length(data.kubectl_path_documents.errbot_network_policy_manifest.documents)
+  yaml_body = element(data.kubectl_path_documents.errbot_network_policy_manifest.documents, count.index)
+}
