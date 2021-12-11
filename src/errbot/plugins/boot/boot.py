559f47dbdbccf6d5dfa99fab280ee10973f2fd71
Grant Birkinbine <grantbirki@github.com>
2021-12-11 14:34:47 -0800

    enable STATUS_PUSH_ENDPOINT

--- src/errbot/plugins/boot/boot.py
+++ src/errbot/plugins/boot/boot.py
@@ -1,5 +1,6 @@
 from errbot import BotPlugin, botcmd
 import os
+import requests
 
 # Version of the message that's triggered after installing the plugin
 # Incrementing this ensures the message is re-triggered, even if it had
@@ -7,7 +8,9 @@ import os
 INSTALL_MESSAGE_VERSION = 1
 # Message text to send to bot admins upon installing/updating plugin
 INSTALL_MESSAGE_TEXT = "🟢 Systems are now online"
-
+# Interval for pushing health checks to the status_page endpoint
+INTERVAL = 25
+STATUS_PUSH_ENDPOINT = os.environ.get("STATUS_PUSH_ENDPOINT", False)
 
 class Boot(BotPlugin):
     """Boot file for starting the chatbot and sending a status message to admins"""
@@ -20,6 +23,15 @@ class Boot(BotPlugin):
         ):
             self.warn_admins(INSTALL_MESSAGE_TEXT)
             self["INSTALL_MESSAGE_VERSION"] = INSTALL_MESSAGE_VERSION
+
+        if not STATUS_PUSH_ENDPOINT:
+            self.log.warn("STATUS_PUSH_ENDPOINT is disabled")
+        else:
+            self.log.info(f"STATUS_PUSH_ENDPOINT is configured to: {STATUS_PUSH_ENDPOINT}")
+            self.start_poller(INTERVAL, self.push_health_status)
+
+    def push_health_status(self):
+        requests.get(STATUS_PUSH_ENDPOINT)
 
     @botcmd(admin_only=True)
     def sentry(self, mess, args):
