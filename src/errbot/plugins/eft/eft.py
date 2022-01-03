c5dfe0a4d319ac7ddf23aaa38be9e794a2612aab
Grant Birkinbine <grantbirki@github.com>
2022-01-02 17:04:40 -0700

    add and fix eft methods

--- src/errbot/plugins/eft/eft.py
+++ src/errbot/plugins/eft/eft.py
@@ -1,6 +1,10 @@
+import re
+from string import Template
+
 import requests
 from errbot import BotPlugin, botcmd
 from lib.chat.chatutils import ChatUtils
+from lib.common.errhelper import ErrHelper
 
 chatutils = ChatUtils()
 
@@ -16,6 +20,15 @@ class Eft(BotPlugin):
         Syntax: .eft <item name>
         Example: .eft gas an -> will get the price of a Gas Analyzer
         """
+        ErrHelper().user(msg)
+
+        # Set the type of args to a string
+        args = str(args).strip()
+
+        # Validate the input
+        if not self.input_validation(args):
+            self.general_error(msg, "Invalid input. Please try again.")
+            return
 
         # Execute the graphql query to try and get eft item data
         result = self.graph_ql(self.item_query(args))
@@ -38,23 +51,99 @@ class Eft(BotPlugin):
         # Format the types to be wrapped in backticks to look pretty
         types = self.fmt_item_types(result_data)
 
+        # Get the highest trader price and the trader name
+        highest_price, trader = self.get_highest_trader_price(result_data)
+
+        # Get item 'tier'
+        # TODO
+
+        # Send a successful card with the eft item data
+        body = ""
+        body += f"**Price and Item Details:**\n"
+        body += f"• Wiki: {result_data['link']}\n"
+        body += f"• Sell to: `{trader}` for `{self.fmt_number(highest_price)}`\n"
+
+        # Get Average Flea Price
+        flea_price = self.fmt_number(result_data["avg24hPrice"])
+        if flea_price == 0 or flea_price == "0":
+            flea_price = "N/A"
+
         # Send a successful card with the eft item data
         self.send_card(
             title=result_data["name"],
-            body="Price and Item Details:",
-            link=result_data["link"],
+            body=body,
             color=chatutils.color("white"),
             in_reply_to=msg,
             thumbnail=result_data["iconLink"],
             fields=(
-                ("Avg. 24h Price:", result_data["avg24hPrice"]),
-                ("Base Price:", result_data["basePrice"]),
-                ("Change Last 48h:", result_data["changeLast48hPercent"]),
-                ("Types:", types),
+                ("Avg Flea Price:", flea_price),
+                ("Base Price:", self.fmt_number(result_data["basePrice"])),
+                (
+                    "Change Last 48h:",
+                    self.get_price_change(result_data["changeLast48hPercent"]),
+                ),
+                ("Item Types:", types),
             ),
         )
-
         return
+
+    def get_item_tier(self, result_data):
+        """not implemented yet"""
+        return
+
+    def get_highest_trader_price(self, result_data):
+        """
+        Helper function for getting the highest trader price.
+        :param result_data: The result of the graphql query
+        :return: The highest trader price (int) and the trader name (string)
+        """
+        highest_price = 0
+        trader = ""
+        for price in result_data["sellFor"]:
+            if price["price"] > highest_price:
+                highest_price = int(price["price"])
+                trader = str(price["source"])
+        return highest_price, trader
+
+    def fmt_number(self, number):
+        """
+        Helper function to format a number to a string with commas.
+        :param number: The number to format (int)
+        :return: The formatted number (string)
+        """
+        return "{:,}".format(number)
+
+    def get_price_change(self, price_change):
+        """
+        Helper function for formatting the price change.
+        :param price_change: The price change (float)
+        :return: The formatted price change (string)
+        """
+        if not price_change:
+            return "N/A"
+
+        if price_change > 0:
+            return f"{price_change}% 📈"
+        elif price_change < 0:
+            return f"{price_change}% 📉"
+        else:
+            return "0%"
+
+    def input_validation(self, args):
+        """
+        Helper function for validating the user input.
+        :param args: The user input (string)
+        :return: True if the input is valid, False if not
+        """
+        # Character Allow List
+        regex = r"^[a-zA-Z\d\s-]+$"
+
+        # If there is a match the input is valid and we can return true
+        if re.match(regex, args):
+            return True
+        # If there is no match, the input is invalid and we can return false
+        else:
+            return False
 
     def graph_ql(self, query):
         """
@@ -84,26 +173,32 @@ class Eft(BotPlugin):
         types = types[:-2]
         return types
 
-    def item_query(self, args):
+    def item_query(self, name):
         """
         Helper function for building the graphql query for an eft item.
         """
-        query = """
+        query = Template(
+            """
             {
-                itemsByName(name: "{}") {
+                itemsByName(name: "$name") {
                     name
                     types
                     avg24hPrice
                     basePrice
+                    width
+                    height
                     changeLast48hPercent
                     iconLink
                     link
+                    sellFor {
+                        price
+                        source
+                    }
                 }
             }
-            """.format(
-            args
+            """
         )
-        return query
+        return query.substitute(name=name)
 
     def general_error(self, src_msg, msg):
         """
