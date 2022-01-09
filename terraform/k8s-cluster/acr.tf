1514f6f0becec1f320526ceec34591c161a10744
Grant Birkinbine <grantbirki@github.com>
2022-01-09 10:02:31 -0700

    use admin enabled acr

--- terraform/k8s-cluster/acr.tf
+++ terraform/k8s-cluster/acr.tf
@@ -10,7 +10,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.default.name
   location            = var.CLOUD_LOCATION
   sku                 = "Basic"
-  admin_enabled       = false
+  admin_enabled       = true
 
   tags = {
     managed_by = "terraform"
