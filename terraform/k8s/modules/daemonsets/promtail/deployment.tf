7ef2cb4fadf25c9c6dce8d255514e59293ab4939
Grant Birkinbine <grantbirki@github.com>
2022-02-11 00:04:19 -0800

    add kubectl_path_documents due to typo

--- terraform/k8s/modules/daemonsets/promtail/deployment.tf
+++ terraform/k8s/modules/daemonsets/promtail/deployment.tf
@@ -1,13 +1,13 @@
 data "kubectl_path_documents" "promtail_deployment_manifest" {
   depends_on = [
-    data.kubectl_file_documents.promtail_configmap_manifest
+    data.kubectl_path_documents.promtail_configmap_manifest
   ]
   pattern = "modules/daemonsets/promtail/deployment.yaml"
 }
 
 resource "kubectl_manifest" "promtail_deployment" {
   depends_on = [
-    data.kubectl_file_documents.promtail_configmap_manifest
+    data.kubectl_path_documents.promtail_configmap_manifest
   ]
   count     = length(data.kubectl_path_documents.promtail_deployment_manifest.documents)
   yaml_body = element(data.kubectl_path_documents.promtail_deployment_manifest.documents, count.index)
