2a0b82111ec2fc4b9855b2989b781d82732d395e
Grant Birkinbine <grantbirki@github.com>
2022-02-10 23:08:19 -0800

    add totals bot command to view command totals

--- src/errbot/plugins/example/example.py
+++ src/errbot/plugins/example/example.py
@@ -1,17 +1,23 @@
 import os
+import json
 
 import psutil
 from errbot import BotPlugin, botcmd
 from lib.chat.chatutils import ChatUtils
 from lib.chat.discord_custom import DiscordCustom
+from lib.database.dynamo import Dynamo
+from lib.database.dynamo_tables import BotDataTable
 
 from lib.common.errhelper import ErrHelper
 
 chatutils = ChatUtils()
+dynamo = Dynamo()
 
 STATUS_PAGE_URL = os.environ.get("STATUS_PAGE_URL", False)
 DOCS_URL = os.environ.get("DOCS_URL", False)
 BACKEND = os.environ["BACKEND"]
+BOT_NAME = os.environ["BOT_NAME"].strip()
+BOT_PREFIX = os.environ.get("BOT_PREFIX", ".")
 
 
 class Example(BotPlugin):
@@ -166,8 +172,23 @@ class Example(BotPlugin):
     def totals(self, msg, args):
         """Get all the totals for the bots usage"""
 
-        message = f"• TODO\n"
+        # Attempt to get the bot data table for this bot
+        record = dynamo.get(BotDataTable, BOT_NAME)
 
+        # If the record exists, update it with the most recent values collected
+        if record:
+            record_parsed = json.loads(record.command_usage_data)
+        elif record is None:
+            return "❌ I could not find a record for this bot in the database. Please contact the bot owner."
+        elif record is False:
+            return "❌ I failed to get the bot usage totals from the database for this bot. Please contact the bot owner."
+
+        # Format the totals message
+        message = f"**Command Usage Totals:**\n"
+        for key, value in record_parsed.items():
+            message += f"• `{BOT_PREFIX}{key}` **: {value}**\n"
+
+        # Return a card with the totals message
         chatutils.send_card_helper(
             bot_self=self,
             title="📊 Bot Usage Totals",
