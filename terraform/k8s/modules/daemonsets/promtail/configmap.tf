e162658d21774afc5c43706c325be05e5c6c409a
Grant Birkinbine <grantbirki@github.com>
2022-02-11 18:33:00 -0800

    noop change

--- terraform/k8s/modules/daemonsets/promtail/configmap.tf
+++ terraform/k8s/modules/daemonsets/promtail/configmap.tf
@@ -9,7 +9,7 @@ resource "kubectl_manifest" "promtail_configmap" {
   count     = length(data.kubectl_path_documents.promtail_configmap_manifest.documents)
   yaml_body = element(data.kubectl_path_documents.promtail_configmap_manifest.documents, count.index)
 
-  # hide all the output from the yaml_body as it contains sensitive data
+  # hide all the output from the yaml_body as it contains sensitive data (LOKI_PUSH_URL is a secret)
   # ref: https://registry.terraform.io/providers/gavinbunney/kubectl/latest/docs/resources/kubectl_manifest#sensitive-fields
   sensitive_fields = ["data"]
 }
