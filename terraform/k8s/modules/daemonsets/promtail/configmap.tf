899ebeeb57c560f5d3f51fb173d745068100a4fd
Grant Birkinbine <grantbirki@github.com>
2022-02-11 18:22:41 -0800

    fix sensitive_fields

--- terraform/k8s/modules/daemonsets/promtail/configmap.tf
+++ terraform/k8s/modules/daemonsets/promtail/configmap.tf
@@ -1,6 +1,6 @@
 data "kubectl_path_documents" "promtail_configmap_manifest" {
   sensitive_vars = {
-    LOKI_PUSH_URL            = "${var.LOKI_PUSH_URL}"
+    LOKI_PUSH_URL = "${var.LOKI_PUSH_URL}"
   }
   pattern = "modules/daemonsets/promtail/promtail-config.yaml"
 }
@@ -8,4 +8,8 @@ data "kubectl_path_documents" "promtail_configmap_manifest" {
 resource "kubectl_manifest" "promtail_configmap" {
   count     = length(data.kubectl_path_documents.promtail_configmap_manifest.documents)
   yaml_body = element(data.kubectl_path_documents.promtail_configmap_manifest.documents, count.index)
+
+  # hide all the output from the yaml_body as it contains sensitive data
+  # ref: https://registry.terraform.io/providers/gavinbunney/kubectl/latest/docs/resources/kubectl_manifest#sensitive-fields
+  sensitive_fields = ["data"]
 }
--- terraform/k8s/modules/daemonsets/promtail/deployment.tf
+++ terraform/k8s/modules/daemonsets/promtail/deployment.tf
@@ -11,8 +11,4 @@ resource "kubectl_manifest" "promtail_deployment" {
   ]
   count     = length(data.kubectl_path_documents.promtail_deployment_manifest.documents)
   yaml_body = element(data.kubectl_path_documents.promtail_deployment_manifest.documents, count.index)
-
-  # hide all the output from the yaml_body as it contains sensitive data
-  # ref: https://registry.terraform.io/providers/gavinbunney/kubectl/latest/docs/resources/kubectl_manifest#sensitive-fields
-  sensitive_fields = "data" 
 }
