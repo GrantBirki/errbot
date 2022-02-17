9f9be13b7ed7f9e89a188bbe9e04c17f02469f97
Grant Birkinbine <grantbirki@github.com>
2022-02-16 19:07:56 -0800

    add server ban logic

--- src/errbot/plugins/lib/common/ban.py
+++ src/errbot/plugins/lib/common/ban.py
@@ -12,22 +12,28 @@ util = Util()
 LOG = logging.getLogger(__name__)
 BOT_NAME = os.environ["BOT_NAME"].strip()
 BAN_USER_RECORD = "user-bans"
+BAN_SERVER_RECORD = "server-bans"
 
 
 class Ban:
-    def remove_user(self, user):
+    def remove_ban(self, ban, ban_type="user"):
         """
-        Remove a ban for a given user
-        :param user: The handle of the user to un-ban
+        Remove a ban from the remote state
+        :param ban: The user or server to remove from the ban list
+        :param ban_type: The type of ban to remove. "user" or "server"
         :return: True if successful, False if it fails
         """
-        # Attempt to get the user bans record
-        record = dynamo.get(BotDataTable, BAN_USER_RECORD)
+        # Attempt to get the ban record
+        if ban_type == "user":
+            ban_record = BAN_USER_RECORD
+        elif ban_type == "server":
+            ban_record = BAN_SERVER_RECORD
+        record = dynamo.get(BotDataTable, ban_record)
 
-        # If the record exists, update it with the user removal
+        # If the record exists, update it with the removal
         if record:
             record_parsed = json.loads(record.value)
-            record_parsed.remove(user)
+            record_parsed.remove(ban)
 
             # Update the record with the latest data
             update_result = dynamo.update(
@@ -39,37 +45,42 @@ class Ban:
             # If the update was successful, return
             if update_result:
                 # uncomment the line(s) below for debugging and verbosity
-                LOG.info(f'Successfully removed ban for "{user}"')
+                LOG.info(f'Successfully removed ban for "{ban}"')
                 return True
             # If the update failed due to a missing record, log the error
             elif update_result is None:
-                LOG.error(f'Failed to remove ban for "{user}" due to a missing record')
+                LOG.error(f'Failed to remove ban for "{ban}" due to a missing record')
                 return False
             # If the update failed, log an error and return
             elif update_result is False:
-                LOG.error(f'Failed to remove ban for "{user}"')
+                LOG.error(f'Failed to remove ban for "{ban}"')
                 return False
 
         # If the record doesn't exist, exit
         elif record is None:
             LOG.info(
-                f'Could not remove ban for "{user}" because the "{BAN_USER_RECORD}" record was not found"'
+                f'Could not remove ban for "{ban}" because the "{BAN_USER_RECORD}" record was not found"'
             )
             return False
 
-    def user(self, user):
+    def add_ban(self, ban, ban_type="user"):
         """
-        Ban a given user
-        :param user: The handle of the user to ban
+        Ban a given user or server
+        :param ban: The user or server to add to the ban list
+        :param ban_type: The type of ban to remove. "user" or "server"
         :return: True if successful, False if it fails
         """
-        # Attempt to get the user bans record
-        record = dynamo.get(BotDataTable, BAN_USER_RECORD)
+        # Attempt to get the ban record
+        if ban_type == "user":
+            ban_record = BAN_USER_RECORD
+        elif ban_type == "server":
+            ban_record = BAN_SERVER_RECORD
+        record = dynamo.get(BotDataTable, ban_record)
 
         # If the record exists, update it with the most recent values collected
         if record:
             record_parsed = json.loads(record.value)
-            record_parsed.append(user)
+            record_parsed.append(ban)
 
             # Update the record with the latest data
             update_result = dynamo.update(
@@ -81,49 +92,53 @@ class Ban:
             # If the update was successful, return
             if update_result:
                 # uncomment the line(s) below for debugging and verbosity
-                LOG.info(f'Successfully updated banned users record with "{user}"')
+                LOG.info(f'Successfully updated ban record with "{ban}"')
                 return True
             # If the update failed due to a missing record, log the error
             elif update_result is None:
                 LOG.error(
-                    f'Failed to update banned users record with "{user}" due to a missing record'
+                    f'Failed to update ban record with "{ban}" due to a missing record'
                 )
                 return False
             # If the update failed, log an error and return
             elif update_result is False:
-                LOG.error(f'Failed to update banned users record with "{user}"')
+                LOG.error(f'Failed to update ban record with "{ban}"')
                 return False
 
         # If the record doesn't exist, create it
         elif record is None:
             LOG.info(
-                f'BotDataTable record for "{BAN_USER_RECORD}" was not found. Creating..."'
+                f'BotDataTable record for "{ban_record}" was not found. Creating..."'
             )
             new_record = dynamo.write(
                 BotDataTable(
-                    key=BAN_USER_RECORD,
-                    value=json.dumps([user]),
+                    key=ban_record,
+                    value=json.dumps([ban]),
                     updated_at=util.iso_timestamp(),
                 )
             )
             # If the write was successful, log and return
             if new_record:
-                LOG.info(f'Created new BotDataTable record for "{BAN_USER_RECORD}"')
+                LOG.info(f'Created new BotDataTable record for "{ban_record}"')
                 return True
             # If the write failed, log an error and return
             elif new_record is False:
-                LOG.error(f'Failed to write to BotDataTable for "{BAN_USER_RECORD}"')
+                LOG.error(f'Failed to write to BotDataTable for "{ban_record}"')
                 return False
 
-    def get_banned_users(self):
+    def get_bans(self, ban_type="user"):
         """
-        Gets a list of all banned users from the remote state
-        :return: A list of all banned users
-
+        Gets a list of all bans from the remote state (users or servers)
+        :param ban_type: The type of bans to get. "user" or "server"
+        :return: A list of all bans
         Note: If the record does not exist, it will be created as an empty list []
         """
-        # Attempt to get the user bans record
-        record = dynamo.get(BotDataTable, BAN_USER_RECORD)
+        # Attempt to get the ban record
+        if ban_type == "user":
+            ban_record = BAN_USER_RECORD
+        elif ban_type == "server":
+            ban_record = BAN_SERVER_RECORD
+        record = dynamo.get(BotDataTable, ban_record)
 
         # If the record exists, return it
         if record:
@@ -133,21 +148,21 @@ class Ban:
         # Note: This should only ever happen once, when the ban record does not exist
         elif record is None:
             LOG.warning(
-                f'BotDataTable record for "{BAN_USER_RECORD}" was not found. Creating..."'
+                f'BotDataTable record for "{ban_record}" was not found. Creating..."'
             )
             new_record = dynamo.write(
                 BotDataTable(
-                    key=BAN_USER_RECORD,
+                    key=ban_record,
                     value=json.dumps([]),  # create a brand new empty record
                     updated_at=util.iso_timestamp(),
                 )
             )
             # If the write was successful, log and return
             if new_record:
-                LOG.info(f'Created new BotDataTable record for "{BAN_USER_RECORD}"')
+                LOG.info(f'Created new BotDataTable record for "{ban_record}"')
                 # Return the empty list as the record was just created
                 return []
             # If the write failed, log an error and return
             elif new_record is False:
-                LOG.error(f'Failed to write to BotDataTable for "{BAN_USER_RECORD}"')
+                LOG.error(f'Failed to write to BotDataTable for "{ban_record}"')
                 return False
