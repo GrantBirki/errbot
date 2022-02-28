cec31b4c19a4c6960dd43a9e5cfe3e3d26c37a72
Grant Birkinbine <grantbirki@github.com>
2022-02-27 22:04:14 -0800

    new test with requests_mock.get

---
+++ tests/plugins/test_crypto.py
@@ -0,0 +1,8 @@
+pytest_plugins = ["errbot.backends.test"]
+
+extra_plugin_dir = "./src/errbot/plugins"
+
+def test_crypto(requests_mock, testbot):
+    requests_mock.get('https://min-api.cryptocompare.com/data/price?fsym=btc&tsyms=USD', json={'USD': '1.00'})
+    testbot.push_message("!crypto btc")
+    assert "$1.00" in testbot.pop_message()
