3498b7a5ac9766ba20f7d7354f7efc9c21d370b7
Grant Birkinbine <grantbirki@github.com>
2022-02-27 22:58:33 -0800

    lint

--- tests/plugins/test_crypto.py
+++ tests/plugins/test_crypto.py
@@ -11,10 +11,14 @@ def test_crypto(requests_mock, testbot):
     testbot.push_message("!crypto btc")
     assert "$1.00" in testbot.pop_message()
 
+
 def test_crypto_with_error(requests_mock, testbot):
     requests_mock.get(
         "https://min-api.cryptocompare.com/data/price?fsym=evilcrypto&tsyms=USD",
-        json={"Response":"Error","Message":"market does not exist for this coin pair (EVILCRYPTO-USD)"},
+        json={
+            "Response": "Error",
+            "Message": "market does not exist for this coin pair (EVILCRYPTO-USD)",
+        },
     )
     testbot.push_message("!crypto evilcrypto")
     assert "not a valid crypto currency" in testbot.pop_message()
