cc70fcea0310a9e53035c676fa6822e4feff23eb
Grant Birkinbine <grant.birkinbine@gmail.com>
2022-04-27 20:45:38 -0400

    disable public container

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -17,24 +17,24 @@ module "errbot" {
   DISABLE_LEAGUE_CRON = "False"
 }
 
-module "errbot_public" {
-  source = "./modules/containers/errbot"
-  # Environment variables
-  IMAGE_TAG = var.IMAGE_TAG
-  # Bot Creds
-  CHAT_SERVICE_TOKEN    = var.CHAT_SERVICE_TOKEN_PUBLIC
-  AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID_ENCODED
-  AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY_ENCODED
-  RIOT_TOKEN            = var.RIOT_TOKEN
-  SPOTIFY_CLIENT_ID     = var.SPOTIFY_CLIENT_ID
-  SPOTIFY_CLIENT_SECRET = var.SPOTIFY_CLIENT_SECRET
-  SENTRY                = var.SENTRY
+# module "errbot_public" {
+#   source = "./modules/containers/errbot"
+#   # Environment variables
+#   IMAGE_TAG = var.IMAGE_TAG
+#   # Bot Creds
+#   CHAT_SERVICE_TOKEN    = var.CHAT_SERVICE_TOKEN_PUBLIC
+#   AWS_ACCESS_KEY_ID     = var.AWS_ACCESS_KEY_ID_ENCODED
+#   AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY_ENCODED
+#   RIOT_TOKEN            = var.RIOT_TOKEN
+#   SPOTIFY_CLIENT_ID     = var.SPOTIFY_CLIENT_ID
+#   SPOTIFY_CLIENT_SECRET = var.SPOTIFY_CLIENT_SECRET
+#   SENTRY                = var.SENTRY
 
-  # Config
-  ACR_NAME            = data.azurerm_container_registry.acr.name
-  NAMESPACE           = "errbot-public"
-  DISABLE_LEAGUE_CRON = "True"
-}
+#   # Config
+#   ACR_NAME            = data.azurerm_container_registry.acr.name
+#   NAMESPACE           = "errbot-public"
+#   DISABLE_LEAGUE_CRON = "True"
+# }
 
 module "promtail" {
   source = "./modules/daemonsets/promtail"
